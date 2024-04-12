1715d369bb5ef0a60fd52c56e47f89cd72b4d1dc
FerCesio <162721896+FerCesio@users.noreply.github.com>
2024-04-12 11:11:31 -0300

    Update line.py

--- line.py
+++ line.py
@@ -7,12 +7,12 @@ def line():
     y1 = float(A*x1+B)
     y2 = float(A*x2+B)
     print(f"""
-    El coeficiente A de su ecuacion de la recta es: {A}
-    El coeficiente B de su ecuacion de la recta es: {B}
-    El coeficiente X1 de su ecuacion de la recta es: {x1}
-    El coeficiente X2 de su ecuacion de la recta es: {x2}
+    El coeficiente A de su ecuación de la recta es: {A}
+    El coeficiente B de su ecuación de la recta es: {B}
+    El coeficiente X1 de su ecuación de la recta es: {x1}
+    El coeficiente X2 de su ecuación de la recta es: {x2}
     
-    Para la siguiente ecuacion:
+    Para la siguiente ecuación:
             Y = {A}x + {B}
     
     Dados los siguientes puntos:
